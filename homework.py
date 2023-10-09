695260c4c7d3e703f949e3d478132efe69089133
Irina Kirillova <tokkirin@gmail.com>
2023-10-09 13:59:11 +0600

    fix 3

--- homework.py
+++ homework.py
@@ -5,10 +5,9 @@ def test_dark_theme_by_time():
     """
     Протестируйте правильность переключения темной темы на сайте в зависимости от времени
     """
-    current_time = time(hour=23)
+    current_time = time(hour=5)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    is_dark_theme = None
     if 6 <= current_time.hour < 22:
         is_dark_theme = False
     else:
@@ -24,8 +23,8 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
-    current_time = time(hour=16)
-    dark_theme_enabled_by_user = True
+    current_time = time(hour=5)
+    dark_theme_enabled_by_user = None
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
     is_dark_theme = None
@@ -34,10 +33,10 @@ def test_dark_theme_by_time_and_user_choice():
     elif dark_theme_enabled_by_user is False:
         is_dark_theme = False
     elif dark_theme_enabled_by_user is None:
-        if 6 > current_time.hour >= 22:
-            is_dark_theme = True
-        else:
+        if 6 <= current_time.hour < 22:
             is_dark_theme = False
+        else:
+            is_dark_theme = True
     assert is_dark_theme is True
 
 
@@ -84,6 +83,7 @@ def best_name(function_name, *args):
     func_name = function_name.__name__.replace('_', ' ').capitalize()
     res = f"{func_name} [{', '.join(args)}]"
     print(res)
+    return res
 
 
 def test_readable_function():
